edba23aa52e99722f0f5bd63206428da20d5302c
code@brilaps.com <code@brilaps.com@2dac48d0-7b47-0410-a1a9-7f54aa4e1825>
2008-03-23 01:17:37 +0000

    Adding a credits file... giving credit where it's due

---
+++ trunk/CREDITS.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Mia relies on a number of great open source applications/library's to function.  It's only fair that we give credit where credit is due.
+ * So below is a list of other open source tools we are relying on...
+ *
+ * Code:
+ * jQuery - http://jquery.com
+ * jQuery Plugins:
+ * - splitter: http://methvin.com/jquery/splitter/
+ * - timer - http://www.mattptr.net
+ * - greybox redux - written by: John Resig, based on code by: 4mir Salihefendic (http://amix.dk)
+ * HTMLPurifier - http://htmlpurifier.org/
+ * ADOdb PHP - http://adodb.sourceforge.net/
+ * PHP5 CAPTCHA - http://sourceforge.net/projects/php5captcha/
+ * Services_JSON - http://pear.php.net/pepr/pepr-proposal-show.php?id=198
+ *
+ * Graphics:
+ * Tango - http://tango.freedesktop.org/Tango_Desktop_Project
+ * famfamfam_silk - http://www.famfamfam.com/lab/icons/silk/
+ */
+?>
