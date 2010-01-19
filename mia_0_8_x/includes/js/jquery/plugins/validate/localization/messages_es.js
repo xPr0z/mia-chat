667a34ed8a835956e60d8614b4ff7c75bc4a0a79
code <code@brilaps.com>
2010-01-19 17:50:37 +0000

    More updates to the jQuery Validation plugin

--- mia_0_8_x/includes/js/jquery/plugins/validate/localization/messages_es.js
+++ mia_0_8_x/includes/js/jquery/plugins/validate/localization/messages_es.js
@@ -1,7 +1,6 @@
 /*
  * Translated default messages for the jQuery validation plugin.
- * Language: ES
- * Author: David Esperalta - http://www.dec.gesbit.com/
+ * Locale: ES
  */
 jQuery.extend(jQuery.validator.messages, {
   required: "Este campo es obligatorio.",
@@ -10,17 +9,15 @@ jQuery.extend(jQuery.validator.messages, {
   url: "Por favor, escribe una URL válida.",
   date: "Por favor, escribe una fecha válida.",
   dateISO: "Por favor, escribe una fecha (ISO) válida.",
-  dateDE: "Bitte geben Sie ein gültiges Datum ein.",
   number: "Por favor, escribe un número entero válido.",
-  numberDE: "Bitte geben Sie eine Nummer ein.",
   digits: "Por favor, escribe sólo dígitos.",
   creditcard: "Por favor, escribe un número de tarjeta válido.",
   equalTo: "Por favor, escribe el mismo valor de nuevo.",
   accept: "Por favor, escribe una valor con una extensión aceptada.",
-  maxlength: jQuery.format("Por favor, no escribas más de {0} caracteres."),
-  minlength: jQuery.format("Por favor, no escribas menos de {0} caracteres."),
-  rangelength: jQuery.format("Por favor, escribe un valor entre {0} y {1} caracteres."),
-  range: jQuery.format("Por favor, escribe un valor entre {0} y {1}."),
-  max: jQuery.format("Por favor, escribe un valor igual o menor que {0}."),
-  min: jQuery.format("Por favor, escribe un valor igual o mayor que {0}.")
+  maxlength: jQuery.validator.format("Por favor, no escribas más de {0} caracteres."),
+  minlength: jQuery.validator.format("Por favor, no escribas menos de {0} caracteres."),
+  rangelength: jQuery.validator.format("Por favor, escribe un valor entre {0} y {1} caracteres."),
+  range: jQuery.validator.format("Por favor, escribe un valor entre {0} y {1}."),
+  max: jQuery.validator.format("Por favor, escribe un valor igual o menor que {0}."),
+  min: jQuery.validator.format("Por favor, escribe un valor igual o mayor que {0}.")
 });
